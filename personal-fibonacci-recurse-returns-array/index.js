579202be83962be209ad610e2c3674709a5ba5c7
zkobrinsky <zachkobrinsky@gmail.com>
2021-05-10 15:57:47 -0400

    changed to strict operator

--- personal-fibonacci-recurse-returns-array/index.js
+++ personal-fibonacci-recurse-returns-array/index.js
@@ -1,11 +1,18 @@
-function fibonacci(num, result = [0, 1], index = 1) {
-  if (!Number.isInteger(num)) return null;
-  if (Math.sign(num) === -1) return null;
-  if (num < 2) return result.slice(0, num + 1);
-  if (index == num) return result;
-  return fibonacci(
-    num,
-    result.concat(result[index] + result[index - 1]),
-    index + 1
-  );
+function fibonacci(num) {
+  // nested to protect from dumb user input
+  function innerFibonacci(num, result = [0, 1], index = 1) {
+    if (!Number.isInteger(num)) return null;
+    if (Math.sign(num) === -1) return null;
+    if (num < 2) return result.slice(0, num + 1);
+    if (index === num) return result;
+    return innerFibonacci(
+      num,
+      result.concat(result[index] + result[index - 1]),
+      index + 1
+    );
+  }
+
+  return innerFibonacci(num);
 }
+
+console.log(fibonacci(7, [0, 2], 7));
