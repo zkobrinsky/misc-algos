5b886889abaea97281e49b6510d1ff304865f3a9
zkobrinsky <zachkobrinsky@gmail.com>
2021-06-14 10:00:25 -0400

    cleaner solution

--- leetcode-transpose-matrix/index.js
+++ leetcode-transpose-matrix/index.js
@@ -1,14 +1,24 @@
-var transpose = function (matrix) {
+// let matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+];
+
+function transpose(matrix) {
   let output = [];
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      if (i === 0) {
-        let array = [matrix[i][j]];
-        output.push(array);
-      } else {
-        output[j].push(matrix[i][j]);
-      }
+  for (let i = 0; i < matrix[0].length; i++) {
+    let array = [];
+    for (let j = 0; j < matrix.length; j++) {
+      array.push(matrix[j][i]);
     }
+    output.push(array);
   }
   return output;
-};
+}
+
+console.log(transpose(matrix));
