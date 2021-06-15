2a4e511cf9ef7ef711222758b15aa932e2856f66
zkobrinsky <zachkobrinsky@gmail.com>
2021-06-15 15:16:59 -0400

    refactored with helpers version

--- leetcode-lucky-numbers-in-a-matrix/index.js
+++ leetcode-lucky-numbers-in-a-matrix/index.js
@@ -21,3 +21,36 @@ var luckyNumbers = function (matrix) {
   }
   return output;
 };
+
+// refactored with helpers:
+// var luckyNumbers = function (matrix) {
+//   let output = [];
+
+//   for (let i = 0; i < matrix.length; i++) {
+//     const [min, index] = findMinAndIndex(matrix[i]);
+//     const col = getColFromIndex(index, matrix);
+//     if (Math.max(...col) === min) output.push(min);
+//   }
+//   return output;
+
+//   function findMinAndIndex(arr) {
+//     let min = Infinity;
+//     let index = 0;
+
+//     for (let j = 0; j < arr.length; j++) {
+//       if (arr[j] < min) {
+//         min = arr[j];
+//         index = j;
+//       }
+//     }
+//     return [min, index];
+//   }
+
+//   function getColFromIndex(index, arr) {
+//     let col = [];
+//     for (let i = 0; i < arr.length; i++) {
+//       col.push(arr[i][index]);
+//     }
+//     return col;
+//   }
+// };
