b42152d33dd85cdcfd7a9562ec4cd4c1cb929e13
zkobrinsky <zachkobrinsky@gmail.com>
2021-06-15 11:43:05 -0400

    created solution

---
+++ leetcode-search-a-2d-matrix-ii/index.js
@@ -0,0 +1,36 @@
+let matrix = [
+    [1, 4, 7, 11, 15],
+    [2, 5, 8, 12, 19],
+    [3, 6, 9, 16, 22],
+    [10, 13, 14, 17, 24],
+    [18, 21, 23, 26, 30],
+  ],
+  target = 5;
+//   output: true;
+
+// let matrix = [
+//     [1, 4, 7, 11, 15],
+//     [2, 5, 8, 12, 19],
+//     [3, 6, 9, 16, 22],
+//     [10, 13, 14, 17, 24],
+//     [18, 21, 23, 26, 30],
+//   ],
+//   target = 20;
+// //   output:false
+
+function searchMatrix(matrix, target) {
+  let row = matrix.length - 1;
+  let col = 0;
+
+  while (row >= 0 && col < matrix[0].length) {
+    if (matrix[row][col] > target) {
+      row--;
+    } else if (matrix[row][col] < target) {
+      col++;
+    } else {
+      return true;
+    }
+  }
+
+  return false;
+}
